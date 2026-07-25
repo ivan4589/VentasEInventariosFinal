bb48637e26562879582f5aa81324eac6ff1f5143
ivan4589 <44450163+ivan4589@users.noreply.github.com>
2026-07-24 20:13:12 -0400

    Implementa modulo de administracion

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,26 @@
-import { IsEmail, IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { $Enums } from '../../../generated/prisma/client';
 
 export class CreateUserDto {
   @IsString()
+  @MinLength(2)
+  @MaxLength(120)
   name: string;
 
   @IsEmail()
+  @MaxLength(160)
   email: string;
 
   @IsString()
-  @MinLength(6)
+  @MinLength(8)
+  @MaxLength(72)
   password: string;
 
   @IsOptional()
