6ac57a3c228b37fe9f319388adf81045956f8f79
ivan4589 <44450163+ivan4589@users.noreply.github.com>
2026-07-30 11:36:24 -0400

    Integra alcance de datos en ventas

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { ReportsModule } from '../reports/reports.module';
 import { SalesController } from './sales.controller';
 import { SalesService } from './sales.service';
-import { ReportsModule } from '../reports/reports.module';
 import { WhatsAppService } from './whatsapp.service';
 
 @Module({
-  imports: [ReportsModule],
+  imports: [ReportsModule, AuthModule],
   controllers: [SalesController],
   providers: [SalesService, WhatsAppService],
 })
