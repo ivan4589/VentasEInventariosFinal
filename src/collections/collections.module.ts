a6835d9690161912a1a246e02165a9db9e939867
ivan4589 <44450163+ivan4589@users.noreply.github.com>
2026-07-30 11:38:12 -0400

    Integra alcance de datos en cobranzas

--- src/collections/collections.module.ts
+++ src/collections/collections.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { ReportsModule } from '../reports/reports.module';
 import { CollectionsController } from './collections.controller';
 import { CollectionsService } from './collections.service';
 
 @Module({
-  imports: [ReportsModule],
+  imports: [ReportsModule, AuthModule],
   controllers: [CollectionsController],
   providers: [CollectionsService],
   exports: [CollectionsService],
